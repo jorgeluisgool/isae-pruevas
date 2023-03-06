85b016edb7deb10b74f66a996590c03bf7b56751
Jorge <jorgeluis.gomez.sat@gmail.com>
2023-03-06 12:29:58 -0600

    Agregue el Navbar utilizando Tailwind

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,10 +3,12 @@ import { Route, Routes } from 'react-router-dom'
 import { HomeScreem } from '../dana/pages/HomeScreem'
 import ProyectosPage from '../dana/pages/ProyectosPage'
 import { RegistrosPage } from '../dana/pages/RegistrosPage'
+import { Navbar } from '../ui/components/Navbar'
 
 export const AppRouter = () => {
   return (
     <>
+    <Navbar/>
         <Routes>
             <Route path='home' element={ <HomeScreem/>}/>
             <Route path='proyectos' element={ <ProyectosPage/>}/>

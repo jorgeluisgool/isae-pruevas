02781e4921e910e268b7bc130574a10fddd3e103
Jorge <jorgeluis.gomez.sat@gmail.com>
2023-03-09 09:44:48 -0600

    Agregue un z-10 al div del nav para que no se vean los iconos

--- src/dana/components/BotonMenu.jsx
+++ src/dana/components/BotonMenu.jsx
@@ -27,8 +27,8 @@ export const BotonMenu = ({titulo, icono}) => {
   
   return (
     <>
-        <div className='bg-sky-700 w-20 h-20 object-cover rounded-full border border-indigo-500 text-center'>
-          <span className='text-gray-800 hover:text-gray-400 duration-500 text-6xl text-slate-50 pt-2'>
+        <div className='bg-sky-700 w-20 h-20 object-cover rounded-full border border-sky-500 text-center pt-2'>
+          <span className='hover:text-gray-400 duration-500 text-6xl text-slate-50'>
             <ion-icon name={icono}></ion-icon>
           </span>
           <p className='pt-4 text-center'>{titulo}</p>
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
     let [open,setOpen]=useState(false);
   return (
     <>
-    <div className='shadow-md w-full fixed top-0 left-0'>
+    <div className='z-10 shadow-md w-full fixed top-0 left-0'>
         <div className='md:flex items-center justify-between bg-sky-700 py-4 md:px-10 px-7'>
             <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] text-gray-800'>
                 <Link to='/menu'>
@@ -18,7 +18,7 @@ export const Navbar = () => {
                 <ion-icon name={open ? 'close':'menu'}></ion-icon>
             </div>
             <ul className={`md:flex md:items-center md:pb-0 pb-7 absolute md:static bg-sky-700 md:z-auto z-[-1] left-40 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 opacity-100':'top-[-490px]'} md:opacity-100 opacity-100`}>
-                <li className='md:ml-8 text-xl'>
+                <li className='md:ml-8 text-xl md:my-0 my-7'>
                 <NavLink className='text-slate-50 hover:text-gray-400 duration-500 md:ml-8 text-xl md:my-0 my-7' to='/proyectos'>
                     Proyectos
                 </NavLink>

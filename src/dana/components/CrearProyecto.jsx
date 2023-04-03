0b09dcf8e5f399e4bfa70221006915c42153abda
Jorge <jorgeluis.gomez.sat@gmail.com>
2023-04-03 18:34:08 -0500

    Se valido el formulario, Crear proyecto junto al input selecionar archivo

--- src/dana/components/CrearProyecto.jsx
+++ src/dana/components/CrearProyecto.jsx
@@ -1,84 +1,134 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import { useState } from "react";
 import { Navigate } from "react-router-dom";
 
 export const CrearProyecto = ({excelData, handleFileUpload}) => {
     
-    const [proyecto, setProyecto] = useState('');
-    const [tipo, setTipo] = useState('');
+    // const [proyecto, setProyecto] = useState('');
+    // const [tipo, setTipo] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    const [valor, setValor] = useState("");
 
-        // Navigate ('/camposproyecto', {
-        //     replace: true
-        // })
-        console.log(proyecto, tipo); 
-        console.log(excelData);     
-    }
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+
+    //     // Navigate ('/camposproyecto', {
+    //     //     replace: true
+    //     // })
+    //     console.log(proyecto, tipo); 
+    //     console.log(excelData);     
+    // }
 
     return (
         <>
-        <form onSubmit={handleSubmit}>
-            <div className='bg-slate-50 m-5 px-8 py-5 rounded-3xl border-2 border-[#245A95]'>
-                <label className="text-lg font-medium">
-                    Registrar Proyecto
-                </label>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-                    <div className="mt-8 flex flex-col gap-y-4">
-                        <button className="active:scale-[.98] transition-all py-3 rounded-xl bg-[#245A95] hover:bg-sky-700 text-white text-lg font-bold">
-                            <ion-icon name="document-outline" />
-                        </button>
-                    </div>
-                    <div className="mt-8 flex flex-col gap-y-4 w-full">
-                        <input 
-                            className='w-full border-2 border-gray-300 rounded-xl py-3 bg-transparet'
-                            type='text'
-                            placeholder='Proyecto'
-                            name='proyecto'
-                            value={proyecto}
-                            onChange={(e)=> setProyecto(e.target.value)}
-                        />
-                    </div>
-                    <div className="mt-8 flex flex-col gap-y-4">
-                            <select
-                                className="border-2 border-gray-300 rounded-xl py-3 bg-transparet"
-                                name="tipo"
-                                value={proyecto}
-                            onChange={(e)=> setTipo(e.target.value)}
-                            >
-                                <option value="migraciones">MIGRACIONES</option>
-                                <option value="inventario">INVENTARIO</option>
-                                <option value="mentenimiento">MANTENIMIENTO</option>
-                                <option value="otros">OTROS</option>
-                            </select>
-                    </div>
-                    <div className="mt-8 flex flex-col gap-y-4">
-                        <input 
-                            className="block w-full text-sm text-gray-500
-                            file:mr-4 file:py-2 file:px-4
-                            file:rounded-md file:border-0
-                            file:text-sm file:font-semibold
-                            file:bg-[#245A95] file:text-white
-                            hover:file:bg-sky-700
-                            form-input border border-gray-300 bg-whit py-2 px-3 rounded-lg shadow-sm focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" 
-                            type="file" 
-                            name="agregarArchivo"
-                            accept="application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-                            onChange={handleFileUpload}
-                        />
-                    </div>
-                    
-                    </div>
-                    <div className="flex">
-                        <button
-                          type="submit"
-                          className="ml-auto w-14 h-14 object-cover active:scale-[.98] py-3 bg-[#245A95] hover:bg-sky-700 text-white text-2xl font-bold inline-block rounded-full bg-primary p-2 uppercase leading-normal shadow-md transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] mt-4">
-                          <ion-icon name="arrow-forward"></ion-icon>
-                        </button>
-                    </div>
-                </div>
+        <Formik 
+            initialValues={{
+                proyecto: '',
+                tipo: 'MIGRACIONES',
+                agregarArchivo: '',
+            }}
+            validate={(valores) => {
 
-            </form>
+                let errores = {};
+
+                // Validacion input nombre proyecto
+                if(!valores.proyecto) {
+                    errores.proyecto = 'Ingrese el nombre del proyecto'
+                } else if(!/^[A-Za-z0-9]+$/.test(valores.proyecto)) {
+                    errores.proyecto = 'El nombre del proyecto solo acepta: letras y numeros'
+                }
+
+                if (!valores.agregarArchivo) {
+                    errores.agregarArchivo = "Debes adjuntar un archivo"
+                }
+
+                return errores;
+            }}
+
+            onSubmit={(valores, {resetForm}) => {
+                resetForm();
+                console.log(valores);
+            }}
+        >
+            {({values, errors, handleSubmit, handleChange, handleBlur, setFieldValue}) => (
+                <Form>
+                    <div className='bg-slate-50 m-5 px-8 py-5 rounded-3xl border-2 border-[#245A95]'>
+                    <label className="text-lg font-medium">
+                        Registrar Proyecto
+                    </label>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+                        <div className="mt-8 flex flex-col gap-y-4">
+                            <button className="active:scale-[.98] transition-all py-3 rounded-xl bg-[#245A95] hover:bg-sky-700 text-white text-lg font-bold">
+                                <ion-icon name="document-outline" />
+                            </button>
+                        </div>
+                        <div className="mt-8 flex flex-col gap-y-1 w-full">
+                            <Field 
+                                className='w-full border-2 border-gray-300 rounded-xl py-3 bg-transparet'
+                                type='text'
+                                placeholder='  Proyecto'
+                                name='proyecto'
+                                value={values.proyecto.toUpperCase()}
+                                // onChange={handleChange}
+                                // onBlur={handleBlur}
+                            />
+                            <ErrorMessage name="proyecto" component={() => (
+                                <span className="text-red-600"><ion-icon name="alert-circle-sharp"></ion-icon> {errors.proyecto}</span>
+                            )}/>
+                        </div>
+                        <div className="mt-8 flex flex-col gap-y-4">
+                                <Field
+                                    className="border-2 border-gray-300 rounded-xl py-3 bg-transparet"
+                                    as="select"
+                                    name="tipo"
+                                    // value={values.tipo}
+                                    // onChange={handleChange}
+                                    // onBlur={handleBlur}
+                                >
+                                    <option value="migraciones" defaultValue>MIGRACIONES</option>
+                                    <option value="inventario">INVENTARIO</option>
+                                    <option value="mentenimiento">MANTENIMIENTO</option>
+                                    <option value="otros">OTROS</option>
+                                </Field>
+                                {errors.tipo && <span className="text-red-600">{errors.tipo}</span>}
+                        </div>
+                        <div className="mt-8 flex flex-col gap-y-4">
+                            <input 
+                                className="block w-full text-sm text-gray-500
+                                file:mr-4 file:py-2 file:px-4
+                                file:rounded-md file:border-0
+                                file:text-sm file:font-semibold
+                                file:bg-[#245A95] file:text-white
+                                hover:file:bg-sky-700
+                                form-input border border-gray-300 bg-whit py-2 px-3 rounded-lg shadow-sm focus:outline-none focus:border-blue-500 focus:shadow-outline-blue" 
+                                id="archivo"
+                                type="file" 
+                                name="agregarArchivo"
+                                // value={values.agregarArchivo}
+                                accept="application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                                onChange={(event) => {
+                                    setFieldValue("agregarArchivo", event.currentTarget.files[0]); handleFileUpload(event)
+                                }}
+                            />
+                            <ErrorMessage name="agregarArchivo" component={() => (
+                                <span className="text-red-600"><ion-icon name="alert-circle-sharp"></ion-icon> {errors.agregarArchivo}</span>
+                            )}/>
+                        </div>
+                        
+                        </div>
+                        <div className="flex">
+                            <button
+                              type="submit"
+                              className="ml-auto w-14 h-14 object-cover active:scale-[.98] py-3 bg-[#245A95] hover:bg-sky-700 text-white text-2xl font-bold inline-block rounded-full bg-primary p-2 uppercase leading-normal shadow-md transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] mt-4">
+                              <ion-icon name="arrow-forward"></ion-icon>
+                            </button>
+                        </div>
+                    </div>
+                </Form>
+            )}
+        
+        </Formik>
+        
                 
         </>
     )

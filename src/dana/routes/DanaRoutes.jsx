9f1289ed6c6839449a27bc6fc8fd52cc954f0ee1
jorge luis <jorgeluis_gool@hotmail.com>
2024-01-10 17:54:57 -0600

    avance catalogos 10 enero

--- src/dana/routes/DanaRoutes.jsx
+++ src/dana/routes/DanaRoutes.jsx
@@ -13,32 +13,32 @@ import { EjemploMultiFile } from '../components/ejemploMultiFile'
 import { MultiFile } from '../components/MultiFile'
 import { UsuariosPage } from '../pages/UsuariosPage'
 import { CatalogoPage } from '../pages/Catalogopage'
-<<<<<<< HEAD
 import { CatalogosPage } from '../pages/CatalogosPage'
 import { AsistenciaPage } from '../pages/AsistenciaPage'
-import {AsistenciaPage} from '../pages/AsistenciaPage'
 
 export const DanaRoutes = () => {
+
   return (
     <>
-      <Navbar />
-      <div className="min-h-screen bg-[#E2E2E2] pt-20">
-        <Routes>
-          <Route element={<ProtectedRoutes />}>
-            <Route path="menu" element={<HomeScreem />} />
-            <Route path="proyectos" element={<ProyectosPage />} />
-            <Route path="ejemplo" element={<MultiFile />} />
-            <Route path="camposproyecto" element={<CamposProyectoPage />} />
-            <Route path="registros" element={<RegistrosPage />} />
-            <Route path="clientes" element={<ClientesRegistrosPage />} />
-            <Route path="usuarios" element={<UsuariosPage />} />
-            <Route path="catalogos" element={<CatalogosPage />} />
-            <Route path="catalogo" element={<CatalogoPage />} />
-            <Route path="asistencia" element={<AsistenciaPage />} />
-            <Route path="/" element={<Navigate to="menu" />} />
-          </Route>
-        </Routes>
+    <Navbar/>
+      <div className='min-h-screen bg-[#E2E2E2] pt-20'>
+          <Routes>
+            <Route element={<ProtectedRoutes />}>
+              <Route path='menu' element={ <HomeScreem/>}/>
+              <Route path='proyectos' element={ <ProyectosPage/>}/>
+              <Route path='ejemplo' element={ <MultiFile/>}/>
+              <Route path='camposproyecto' element={ <CamposProyectoPage/>}/>
+              <Route path='registros' element={ <RegistrosPage/>}/>
+              <Route path='clientes' element={ <ClientesRegistrosPage/>}/>
+              <Route path='usuarios' element={ <UsuariosPage />}/>
+              <Route path='catalogos' element={ <CatalogosPage />}/>
+              <Route path='catalogo' element={ <CatalogoPage />}/>
+              <Route path='asistencia' element={<AsistenciaPage/>}/>
+              <Route path='/' element={<Navigate to='menu'/>}/>
+            </Route>
+          </Routes>
+        
       </div>
     </>
-  );
-};
+  )
+}
